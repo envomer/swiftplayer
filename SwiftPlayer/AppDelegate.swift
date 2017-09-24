bdd6bb3a0957290235352fa0a5dc6b26e8f0b94a
Omer Mohamed Ali <blinness@gmail.com>
2017-09-24 12:53:30 +0200

    Initial

---
+++ SwiftPlayer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SwiftPlayer
+//
+//  Created by Omer Mohamed Ali on 24/09/2017.
+//  Copyright © 2017 Omer Mohamed Ali. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
