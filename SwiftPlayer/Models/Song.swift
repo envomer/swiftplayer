c8b59a25278754d21eef85ff2793f82217dbbe6d
Omer Mohamed Ali <blinness@gmail.com>
2017-09-24 13:05:54 +0200

    Add Song model

---
+++ SwiftPlayer/Models/Song.swift
@@ -0,0 +1,13 @@
+//
+//  Song.swift
+//  SwiftPlayer
+//
+//  Created by Omer Mohamed Ali on 24/09/2017.
+//  Copyright © 2017 Omer Mohamed Ali. All rights reserved.
+//
+
+import Cocoa
+
+class Song: NSObject {
+
+}
